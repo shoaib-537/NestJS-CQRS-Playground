d43c694872a7b1d16395615fef4a3b15a1bcebc1
Shoaib M <3445shoaib@gmail.com>
2023-11-28 23:13:02 +0500

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from './entities/person/person';
+import { PersonModule } from './person/person.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1122',
+      database: 'nestjscqrs',
+      entities: [Person],
+    }),
+    PersonModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
